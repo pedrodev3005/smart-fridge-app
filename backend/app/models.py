5c0462bc9a19d0501f82ecb641b4bfc2ed3aa0fa
Sophia Sales <sophia.sales@academico.ifpb.edu.br>
2026-08-29 12:22:10 -0300

    feat: add event history model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,14 +1,21 @@
+from datetime import datetime
+
 from sqlalchemy import (
     CheckConstraint,
+    DateTime,
     Enum as SqlEnum,
     ForeignKey,
     Integer,
     String,
+    func,
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
-from app.enums import ProductCategory
+from app.enums import (
+    InventoryMovementType,
+    ProductCategory,
+)
 
 
 class Product(Base):
@@ -91,6 +98,57 @@ class Inventory(Base):
     quantity: Mapped[int] = mapped_column(
         Integer,
         nullable=False,
+    )
+
+    product: Mapped["Product"] = relationship(
+        "Product",
+    )
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    __table_args__ = (
+        CheckConstraint(
+            "typeof(quantity) = 'integer' AND quantity >= 1",
+            name="ck_events_quantity_valid",
+        ),
+    )
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        index=True,
+    )
+
+    event_type: Mapped[InventoryMovementType] = mapped_column(
+        SqlEnum(
+            InventoryMovementType,
+            values_callable=lambda enum: [item.value for item in enum],
+            native_enum=False,
+            create_constraint=True,
+            name="inventory_event_type",
+        ),
+        nullable=False,
+    )
+
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            "products.id",
+            ondelete="RESTRICT",
+        ),
+        nullable=False,
+    )
+
+    quantity: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+    )
+
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        server_default=func.current_timestamp(),
     )
 
     product: Mapped["Product"] = relationship(
